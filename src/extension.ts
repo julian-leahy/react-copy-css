d767fff625b32adb06e3d4780fe6caeafc4d7e94
Julian Leahy <84889719+leahyjulian@users.noreply.github.com>
2024-02-29 14:31:46 +1300

    Extract and create CSS class declarations - remove duplicates - copy to clipboard

--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,22 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage("No class names found");
         return;
       }
+
+      // Iterate over each name in the classNames array, replacing the 'className="..."' pattern with the actual class name and splitting the result into an array of strings.
+      const allClassNames = classNames.flatMap((name) =>
+        name.replace(/className="([^"]*)"/, "$1").split(" ")
+      );
+
+      // Remove any duplicates, and convert it back to an array.
+      const uniqueClassNames = Array.from(new Set(allClassNames));
+
+      // Create a CSS class declaration for each class name.
+      const cleanedClassNames = uniqueClassNames.map(
+        (className) => "." + className + " {\n\n}"
+      );
+      const classNamesString = cleanedClassNames.join("\n\n");
+
+      await vscode.env.clipboard.writeText(classNamesString);
     }
   );
 
